410d935b95a339a558cca954c7e9e6b19ddd6145
Manoel Capaverde <manoel.capaverde@noesissolutions.com>
2020-04-13 16:13:22 +0200

    Gilded Rose refactor of main method: - Creation of specif gildedRoseItem class to handle everything related to the item update, including all the rules - Usage of new properties in the class to generalize the behavior of some products, giving more freedom to the store owners to add new products - Simplify logic of updateQuality through breaking it in different methods that are more specialized

--- src/main/java/com/gildedrose/GildedRoseItem.java
+++ src/main/java/com/gildedrose/GildedRoseItem.java
@@ -18,12 +18,97 @@ public class GildedRoseItem extends Item {
     this.qualityDegradation = 1;
   }
 
+  public static GildedRoseItem gildedRoseItemBuilder(Item item) {
+    String itemName = item.name;
+    ItemType itemType = GildedRoseItem.getItemTypeByItemName(itemName);
+    int qualityDegradation = GildedRoseItem.getQualityDegradationByItemName(itemName);
+    return new GildedRoseItem(itemName, item.sellIn, item.quality, itemType, qualityDegradation);
+  }
+
+  private static ItemType getItemTypeByItemName(String itemName) {
+    switch (itemName) {
+      case "Sulfuras, Hand of Ragnaros":
+        return ItemType.LEGENDARY_ITEM;
+      case "Backstage passes to a TAFKAL80ETC concert":
+        return ItemType.TICKETS;
+      default:
+        return ItemType.SIMPLE_ITEM;
+    }
+  }
+
+  private static int getQualityDegradationByItemName(String itemName) {
+    switch (itemName) {
+      case "Aged Brie":
+        return -1;
+      case "Conjured Mana Cake":
+        return 2;
+      default:
+        return 1;
+    }
+  }
+
   public ItemType getItemType() {
     return itemType;
   }
 
   public int getQualityDegradation() {
     return qualityDegradation;
+  }
+
+  public int getQuality() {
+    return this.quality;
+  }
+
+  private void setQuality(int qualityValue) {
+    if (qualityValue < 0) {
+      quality = 0;
+    } else if (qualityValue > 50) {
+      quality = 50;
+    } else {
+      quality = qualityValue;
+    }
+  }
+
+  public int getSellIn() {
+    return this.sellIn;
+  }
+
+  private void setSellIn(int sellIn) {
+    this.sellIn = sellIn;
+  }
+
+  public void updateSellInAndQuality() {
+    this.updateSellIn();
+    this.updateQuality();
+  }
+
+  private void updateQuality() {
+    switch (getItemType()) {
+      case LEGENDARY_ITEM:
+        break;
+      case SIMPLE_ITEM:
+        int sellInFactor = getSellIn() >= 0 ? 1 : 2;
+        setQuality(getQuality() - (getQualityDegradation() * sellInFactor));
+        ;
+        break;
+      case TICKETS:
+        if (getSellIn() >= 10) {
+          setQuality(getQuality() + 1);
+        } else if (getSellIn() >= 5) {
+          setQuality(getQuality() + 2);
+        } else if (getSellIn() >= 0) {
+          setQuality(getQuality() + 3);
+        } else {
+          setQuality(0);
+        }
+        break;
+    }
+  }
+
+  private void updateSellIn() {
+    if (!getItemType().equals(ItemType.LEGENDARY_ITEM)) {
+      setSellIn(getSellIn() - 1);
+    }
   }
 
   @Override
